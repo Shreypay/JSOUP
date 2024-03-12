e6d11daaf7f8d11ca05420c81820b59c1f71cd31
Shreypay <39966989+Shreypay@users.noreply.github.com>
2024-03-12 10:49:38 -0700

    Update jsouprun.java

--- jsouprun.java
+++ jsouprun.java
@@ -12,12 +12,16 @@ import org.jsoup.select.Elements;
 
 public class jsouprun {
 	public static void main(String[] args) {
-        String url = "https://www.walmart.com/reviews/product/1946294711?page=4";
+        String url = "https://www.walmart.com/reviews/product/1946294711?page=2";
 
         try {
             Document doc = Jsoup.connect(url).get();
-            FileWriter fileWriter = new FileWriter("amazonReview.csv");
+            FileWriter fileWriter = new FileWriter("non-target.csv");
+            FileWriter Writers = new FileWriter("target.csv");
+
             fileWriter.write("reviewer name, review, rating and if verfied, date\n"); 
+            Writers.write("reviewer name, review, rating and if verfied, date\n"); 
+
             System.out.println("reviewer name, review, rating and if verfied, date\n");
 
             Elements reviews = doc.select("div.w_DHV_.pv3.mv0");
@@ -26,13 +30,19 @@ public class jsouprun {
                 String reviewerName = review.select("div.f6.gray.pr2.mb2").text();
                 String reviewText = review.select("span.tl-m.mb3.db-m").text();
                 String reviewerRating = review.select("div.flex-grow-1").text();
+                
                 String reviewerDate = review.select("div.f7.gray.mt1").text();
+                if (reviewText.contains("camera")) {
+                	Writers.write("\"" + reviewerName + "\", \"" + reviewText + "\", \""+ reviewerRating + "\", \""+ reviewerDate +"  \"\n");
+//                	System.out.println("\"" + reviewerName + "\", \"" + reviewText + "\", \""+ reviewerRating + "\", \""+ reviewerDate +"  \"\n");
+                }
+            	System.out.println("\"" + reviewerName + "\", \"" + reviewText + "\", \""+ reviewerRating + "\", \""+ reviewerDate +"  \"\n");
+            	fileWriter.write("\"" + reviewerName + "\", \"" + reviewText + "\", \""+ reviewerRating + "\", \""+ reviewerDate +"  \"\n");
 
-                fileWriter.write("\"" + reviewerName + "\", \"" + reviewText + "\", \""+ reviewerRating + "\", \""+ reviewerDate +"  \"\n");
-                System.out.println("\"" + reviewerName + "\", \"" + reviewText + "\", \""+ reviewerRating + "\", \""+ reviewerDate +"  \"\n");
+                
             }
     
-
+            Writers.close();
             fileWriter.close();
             System.out.println("Successfully scraped and saved reviews to amazonReviews.csv");
         } catch (IOException e) {
